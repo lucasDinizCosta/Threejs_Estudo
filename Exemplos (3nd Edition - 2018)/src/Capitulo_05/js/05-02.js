732935bc732650cbe4711c64fe76845dc532f74c
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-03-10 18:30:11 -0300

    05.02 - Basic 2D Geometries - Circle

--- Exemplos (3nd Edition - 2018)/src/Capitulo_05/js/05-02.js
+++ Exemplos (3nd Edition - 2018)/src/Capitulo_05/js/05-02.js
@@ -1,4 +1,5 @@
-function init() {
+
+  function init() {
 
     // Utiliza padrões de camera, status e renderizador
     var stats = initStats();
@@ -6,26 +7,27 @@ function init() {
     var camera = initCamera();
   
     // Cria a cena que prende todos os elementos, sejam eles: Luzes, Texturas, Objetos e etc...
-    // Adiciona uma iluminação padrão de cena
     var scene = new THREE.Scene();
     var groundPlane = addLargeGroundPlane(scene);
     groundPlane.position.y = -10;
-    initDefaultLighting(scene);
+
+    initDefaultLighting(scene);     // Adiciona uma iluminação padrão de cena
   
     // Funções de controle dos objetos em cena
     var controls = new function () {
       var self = this;
   
       // Inicia a geometria e material de base a serem controlados pelo menu interativo
+      // applyMeshNormalMaterial é uma funcão da biblioteca "util.js"
+
       this.appliedMaterial = applyMeshNormalMaterial;
       this.castShadow = true;
       this.groundPlaneVisible = true;
-  
-      this.planeGeometry = new THREE.PlaneGeometry(20, 20, 4, 4);   //Width, Height, WidthSegments, HeightSegments
-      this.width = this.planeGeometry.parameters.width;
-      this.height = this.planeGeometry.parameters.height;
-      this.widthSegments = this.planeGeometry.parameters.widthSegments;
-      this.heightSegments = this.planeGeometry.parameters.heightSegments;
+
+      this.radius = 4;
+      this.thetaStart = 0.3 * Math.PI * 2;
+      this.thetaLength = 0.3 * Math.PI * 2;
+      this.segments = 10;
   
       // Função que redesenha e atualiza os controles do menu e recria a geometria.
       this.redraw = function () {
@@ -33,35 +35,35 @@ function init() {
         // Função da "util.js" que cria o objeto 'mesh' dentro de controls com as propriedades basicas
         // Além disso é responsavel por redesenhar e atualizar o objeto e a interface
         redrawGeometryAndUpdateUI(gui, scene, controls, function() {
-          return new THREE.PlaneGeometry(controls.width, controls.height, Math.round(controls.widthSegments), Math.round(controls.heightSegments));
+          return new THREE.CircleGeometry(self.radius, self.segments, self.thetaStart, self.thetaLength);
         });
       };
     };
+    console.log(controls);
   
     // GUI de controle e ajuste de valores especificos da geometria do objeto
     var gui = new dat.GUI();
-    gui.add(controls, 'width', 0, 40).onChange(controls.redraw);
-    gui.add(controls, 'height', 0, 40).onChange(controls.redraw);
-    gui.add(controls, 'widthSegments', 0, 10).onChange(controls.redraw);
-    gui.add(controls, 'heightSegments', 0, 10).onChange(controls.redraw);
+    gui.add(controls, 'radius', 0, 40).onChange(controls.redraw);
+    gui.add(controls, 'segments', 0, 40).onChange(controls.redraw);
+    gui.add(controls, 'thetaStart', 0, 2 * Math.PI).onChange(controls.redraw);
+    gui.add(controls, 'thetaLength', 0, 2 * Math.PI).onChange(controls.redraw);
 
-    // Cria uma seção de materiais aplicados
+    // Adiciona a seção de materiais
     gui.add(controls, 'appliedMaterial', {
       meshNormal: applyMeshNormalMaterial, 
       meshStandard: applyMeshStandardMaterial
-    }).onChange(controls.redraw)
-  
-    gui.add(controls, 'castShadow').onChange(function(e) {controls.mesh.castShadow = e})
-    gui.add(controls, 'groundPlaneVisible').onChange(function(e) {groundPlane.material.visible = e})
+    }).onChange(controls.redraw);
+
+    gui.add(controls, 'castShadow').onChange(function(e) {controls.mesh.castShadow = e});
+    gui.add(controls, 'groundPlaneVisible').onChange(function(e) {groundPlane.material.visible = e});
   
     // Inicializa o primeiro redesenho de modo que tudo é inicializado
     // Ao fazer isso é criado o parametro "mesh" dentro de controls, assim podendo
     // atribuir os materiais desejados
     controls.redraw();
     var step = 0;
-    
 
-    // inicia o loop de animação
+    // Inicia o loop de animação
     render();
     function render() {
       stats.update();
@@ -71,4 +73,4 @@ function init() {
       requestAnimationFrame(render);
       renderer.render(scene, camera);
     }
-}
+  }
