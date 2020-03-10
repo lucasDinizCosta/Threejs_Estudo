273cef55a385ae2ce44c7d0085abbe868f2e6ff1
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-03-10 17:25:14 -0300

    05.01 - Basic 2D Geometries - Plane

---
+++ Exemplos (3nd Edition - 2018)/src/Capitulo_05/js/05-01.js
@@ -0,0 +1,67 @@
+function init() {
+
+    // Utiliza padrões de camera, status e renderizador
+    var stats = initStats();
+    var renderer = initRenderer();
+    var camera = initCamera();
+  
+    // Cria a cena que prende todos os elementos, sejam eles: Luzes, Texturas, Objetos e etc...
+    // Adiciona uma iluminação padrão de cena
+    var scene = new THREE.Scene();
+    var groundPlane = addLargeGroundPlane(scene);
+    groundPlane.position.y = -10;
+    initDefaultLighting(scene);
+  
+    // Funções de controle dos objetos em cena
+    var controls = new function () {
+      var self = this;
+  
+      // Inicia a geometria e material de base a serem controlados pelo menu interativo
+      this.appliedMaterial = applyMeshNormalMaterial;
+      this.castShadow = true;
+      this.groundPlaneVisible = true;
+  
+      this.planeGeometry = new THREE.PlaneGeometry(20, 20, 4, 4);   //Width, Height, WidthSegments, HeightSegments
+      this.width = this.planeGeometry.parameters.width;
+      this.height = this.planeGeometry.parameters.height;
+      this.widthSegments = this.planeGeometry.parameters.widthSegments;
+      this.heightSegments = this.planeGeometry.parameters.heightSegments;
+  
+      // Função que redesenha e atualiza os controles do menu e recria a geometria.
+      this.redraw = function () {
+        redrawGeometryAndUpdateUI(gui, scene, controls, function() {
+          return new THREE.PlaneGeometry(controls.width, controls.height, Math.round(controls.widthSegments), Math.round(controls.heightSegments));
+        });
+      };
+    };
+  
+    // // GUI de controle e ajuste de valores especificos da geometria do objeto
+    var gui = new dat.GUI();
+    gui.add(controls, 'width', 0, 40).onChange(controls.redraw);
+    gui.add(controls, 'height', 0, 40).onChange(controls.redraw);
+    gui.add(controls, 'widthSegments', 0, 10).onChange(controls.redraw);
+    gui.add(controls, 'heightSegments', 0, 10).onChange(controls.redraw);
+
+    // Cria uma seção de materiais aplicados
+    gui.add(controls, 'appliedMaterial', {
+      meshNormal: applyMeshNormalMaterial, 
+      meshStandard: applyMeshStandardMaterial
+    }).onChange(controls.redraw)
+  
+    gui.add(controls, 'castShadow').onChange(function(e) {controls.mesh.castShadow = e})
+    gui.add(controls, 'groundPlaneVisible').onChange(function(e) {groundPlane.material.visible = e})
+  
+    // Inicializa o primeiro redesenho de modo que tudo é inicializado
+    controls.redraw();
+    var step = 0;
+    
+    render();
+    function render() {
+      stats.update();
+      controls.mesh.rotation.y = step+=0.01
+      controls.mesh.rotation.x = step
+      controls.mesh.rotation.z = step
+      requestAnimationFrame(render);
+      renderer.render(scene, camera);
+    }
+}
