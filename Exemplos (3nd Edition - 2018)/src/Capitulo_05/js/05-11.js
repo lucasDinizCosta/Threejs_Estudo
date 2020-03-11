607550cfa19ae1ba841f5e59321cad5450a860ea
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-03-11 16:32:15 -0300

    05.11 - Basic 3D geometric - Polyhedron

--- Exemplos (3nd Edition - 2018)/src/Capitulo_05/js/05-11.js
+++ Exemplos (3nd Edition - 2018)/src/Capitulo_05/js/05-11.js
@@ -1,17 +1,3 @@
-/**
- *          Torus com nó
- * 
- * radialSegments: número de segmentos ao longo do tamanho do torus. Default: 8.0
- * tube: Define o raio do tubo (formato de uma rosquinha). Default: 4.0
- * radius: valor do raio completo do torus. Default: 100.0
- * tubularSegments: Numero de partições usadas no comprimento do torus. Default: 6
- * arc: Controla a angulação que o torus será gerado. Default: 2*PI
- * p: Define a forma do nó. Este valor determina quantas vezes a geometria 
- *  gira em torno de seu eixo de simetria rotacional Default: 2.
- * q: Define a forma do nó. Este valor determina quantas vezes a geometria 
- *  gira em torno de um círculo no interior do torus. Default: 3.
- * heightScale: Permite esticar o nó do torus. Default: 1
- */
 
 function init() {
 
@@ -24,7 +10,7 @@ function init() {
     // Adiciona uma iluminação padrão de cena
     var scene = new THREE.Scene();
     var groundPlane = addLargeGroundPlane(scene);
-    groundPlane.position.y = -30;
+    groundPlane.position.y = -10;
     initDefaultLighting(scene);
   
     // Funções de controle dos objetos em cena
@@ -36,14 +22,9 @@ function init() {
       this.castShadow = true;
       this.groundPlaneVisible = true;
   
-      var baseGeom = new THREE.TorusKnotGeometry();
-  
-      this.radius = baseGeom.parameters.radius ? baseGeom.parameters.radius : 1;
-      this.tube = 0.3;
-      this.radialSegments = baseGeom.parameters.radialSegments ? baseGeom.parameters.radialSegments : 8;
-      this.tubularSegments = baseGeom.parameters.tubularSegments ? baseGeom.parameters.tubularSegments : 64;
-      this.p = 2;
-      this.q = 3;
+      this.radius = 10;
+      this.detail = 0;
+      this.type = 'Icosahedron';
       
       // Função que redesenha e atualiza os controles do menu e recria a geometria.
       this.redraw = function () {
@@ -51,22 +32,52 @@ function init() {
         // Função da "util.js" que cria o objeto 'mesh' dentro de controls com as propriedades basicas
         // Além disso é responsavel por redesenhar e atualizar o objeto e a interface
         redrawGeometryAndUpdateUI(gui, scene, controls, function() {
-          return new THREE.TorusKnotGeometry(controls.radius, controls.tube, Math.round(
-                    controls.tubularSegments), Math.round(controls.radialSegments), Math.round(
-                    controls.p), Math.round(controls.q));
+          var polyhedron;
+          switch (controls.type) {
+                    case 'Icosahedron':
+                      polyhedron = new THREE.IcosahedronGeometry(controls.radius, controls.detail);
+                      break;
+                    case 'Tetrahedron':
+                      polyhedron = new THREE.TetrahedronGeometry(controls.radius, controls.detail);
+                      break;
+                    case 'Octahedron':
+                      polyhedron = new THREE.OctahedronGeometry(controls.radius, controls.detail);
+                      break;
+                    case 'Dodecahedron':
+                      polyhedron = new THREE.DodecahedronGeometry(controls.radius, controls.detail);
+                      break;
+                    case 'Custom':
+                      // (x, y, z) de cada vertice
+                      var vertices = [
+                         1,  1,  1, 
+                        -1, -1,  1, 
+                        -1,  1, -1, 
+                         1, -1, -1
+                      ];
+            
+                      // Essas são as faces que precisam ser criadas a partir dos vértices.
+                      var indices = [
+                        2, 1, 0, 
+                        0, 3, 2, 
+                        1, 3, 0, 
+                        2, 3, 1
+                      ];
+            
+                      polyhedron = new THREE.PolyhedronGeometry(vertices, indices, controls.radius, controls.detail);
+                      break;
+                  }
+  
+          return polyhedron;
         });
       };
     };
   
     // GUI de controle e ajuste de valores especificos da geometria do objeto
     var gui = new dat.GUI();
-    gui.add(controls, 'radius', 0, 10).onChange(controls.redraw);
-    gui.add(controls, 'tube', 0, 10).onChange(controls.redraw);
-    gui.add(controls, 'radialSegments', 0, 400).step(1).onChange(controls.redraw);
-    gui.add(controls, 'tubularSegments', 1, 200).step(1).onChange(controls.redraw);
-    gui.add(controls, 'p', 1, 10).step(1).onChange(controls.redraw);
-    gui.add(controls, 'q', 1, 15).step(1).onChange(controls.redraw);
-  
+    gui.add(controls, 'radius', 0, 40).step(1).onChange(controls.redraw);
+    gui.add(controls, 'detail', 0, 3).step(1).onChange(controls.redraw);
+    gui.add(controls, 'type', ['Icosahedron', 'Tetrahedron', 'Octahedron', 'Dodecahedron', 'Custom']).onChange(controls.redraw);
+
     // Cria uma seção de materiais aplicados
     gui.add(controls, 'appliedMaterial', {
       meshNormal: applyMeshNormalMaterial, 
@@ -81,7 +92,7 @@ function init() {
     // atribuir os materiais desejados
     controls.redraw();
     var step = 0;
-
+    
     // Inicia o loop de animação
     render();
     function render() {
